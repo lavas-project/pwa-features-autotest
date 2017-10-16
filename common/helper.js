2441dd50832ef57356a9c69581e67f74e145b547
clark-t <clarktanglei@163.com>
2017-10-16 16:33:53 +0800

    add helper

--- common/helper.js
+++ common/helper.js
@@ -28,8 +28,43 @@ export async function init() {
  * @return {Promise} promise object to block execute
  */
 export async function reload() {
-    return new Promise(() => {
-        window.location.reload();
-    });
+    return new Promise(location.reload);
 }
 
+/**
+ * attach a handler to an event, and the handler executes at most once per event type
+ *
+ * @param {Element} target event target
+ * @param {string} event event name
+ * @param {Function} fn event handler
+ * @return {Promise} promise
+ */
+export async function one(target, event, fn) {
+    return new Promise((resolve, reject) => {
+        function handler(e) {
+            Promise.resolve(fn(e))
+            .then(() => {
+                target.removeEventListener(event, handler);
+                resolve();
+            })
+            .catch(() => {
+                target.removeEventListener(event, handler);
+                reject();
+            });
+        }
+
+        target.addEventListener(event, handler);
+    })
+}
+
+/**
+ * block the execution for a period of time
+ *
+ * @param {number} duration sleep duration time
+ * @return {Promise} promise
+ */
+export function sleep(duration) {
+    return new Promise(resolve => {
+        setTimeout(resolve, duration);
+    });
+}
