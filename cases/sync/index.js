41df21eaa4a5c1166fe0698eedadb195c1552d1e
liuruoran88 <liuruoran88@163.com>
2017-10-25 15:15:30 +0800

    sync 修改

--- cases/sync/index.js
+++ cases/sync/index.js
@@ -23,22 +23,25 @@ import {log} from 'log';
     const reg = await navigator.serviceWorker.register('./sw-sync.js', {scope: '/cases/sync/'});
     await sleep(3000);
 
-    // sync register
-    try {
-        const tags = await reg.sync.getTags();
-        if (tags.includes('syncEventTest')) {
-            log('exist syncEventTest background');
+    if (reg.sync) {
+         // sync register
+        try {
+            const tags = await reg.sync.getTags();
+            if (tags.includes('syncEventTest')) {
+                log('exist syncEventTest background');
+            }
+            else {
+                reg.sync.register('syncEventTest');
+                log('sync registered');
+            }
         }
-        else {
-            reg.sync.register('syncEventTest');
-            log('sync registered');
+        catch (error) {
+            console.error('It broke (probably sync not supported or flag not enabled)');
+            console.error(error.message);
+            return;
         }
     }
-    catch (error) {
-        console.error('It broke (probably sync not supported or flag not enabled)');
-        console.error(error.message);
-        return;
-    }
+
     await sleep(5000);
     await reg.unregister();
     log('sync: test finish');
